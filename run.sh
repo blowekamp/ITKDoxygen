7db433060ea1135f766f55c085b70fd8c3570c37
Bradley Lowekamp <blowekamp@mail.nih.gov>
2022-01-04 15:13:14 +0000

    Add initial Dockerfile to build ITK Doxygen

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+
+
+SRC_DIR="/work/ITK"
+BLD_DIR="/work/ITK-bld"
+
+# If the SRC_DIR is not mounted, then this tag will be checked out
+GIT_TAG=${GIT_TAG:-"master"}
+
+set -xe
+
+
+if [ ! -d ${SRC_DIR} ]; then
+    ( git clone https://github.com/InsightSoftwareConsortium/ITK.git ${SRC_DIR} &&
+          cd ${SRC_DIR} &&
+          git checkout ${GIT_TAG} &&
+          git apply /tmp/patch/*.patch
+    )
+fi
+
+
+mkdir -p ${BLD_DIR} && \
+    cd ${BLD_DIR} && \
+    cmake -G Ninja\
+          -DITK_BUILD_DOCUMENTATION=ON\
+          -DITK_DOXYGEN_XML:BOOL=ON\
+          ${SRC_DIR} && \
+    cmake --build . --target Documentation && \
+    cd ${BLD_DIR}/Utilities/Doxygen && \
+    tar --exclude=\*.md5 --exclude=\*.map -zcvf /ITKDoxygen.tar.gz ./html && \
+    tar -zcvf /ITKDoxygenXML.tar.gz ./xml
