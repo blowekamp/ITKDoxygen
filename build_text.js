70678fabee068ed4b9c1bb51a85832b15ce24709
blowekamp <blowekamp@bots.github.com>
2022-04-15 02:02:38 +0000

    Automatic Update Fri Apr 15 02:02:38 UTC 2022

--- build_text.js
+++ build_text.js
@@ -1,3 +1,3 @@
 $(function(){
-    document.getElementById("datetime").textContent = "Thu, 14 Apr 2022 01:15:04 +0000"
+    document.getElementById("datetime").textContent = "Fri, 15 Apr 2022 01:06:19 +0000"
 });
--- search_config.php
+++ search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Thu Apr 14 2022 02:22:10 for ITK by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.17 ",
+  'logo' => "Generated on Fri Apr 15 2022 01:59:46 for ITK by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.17 ",
 );
 
 ?>
