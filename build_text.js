8b5d2275e5e71378e2abf0e045dd1c90e79caddc
blowekamp <blowekamp@bots.github.com>
2022-07-02 02:15:11 +0000

    Automatic Update Sat Jul  2 02:15:11 UTC 2022

--- build_text.js
+++ build_text.js
@@ -1,3 +1,3 @@
 $(function(){
-    document.getElementById("datetime").textContent = "Fri, 01 Jul 2022 01:27:13 +0000"
+    document.getElementById("datetime").textContent = "Sat, 02 Jul 2022 01:17:21 +0000"
 });
--- search_config.php
+++ search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "",
-  'logo' => "Generated on Fri Jul 1 2022 02:28:05 for ITK by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.17 ",
+  'logo' => "Generated on Sat Jul 2 2022 02:12:31 for ITK by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.17 ",
 );
 
 ?>
