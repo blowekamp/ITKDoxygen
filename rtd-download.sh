e0e33a7f251ce8346bb960e36efe77b190d25379
Matt McCormick <matt@mmmccormick.com>
2025-01-16 12:41:54 -0500

    BUG: Fix RTD html extraction directory
    Since the tarball already has "html", this results in html/html.

--- rtd-download.sh
+++ rtd-download.sh
@@ -6,10 +6,9 @@ DOC_VERSION=$(if [[ "${READTHEDOCS_GIT_IDENTIFIER}" == *v* ]]; then echo "${READ
 DOC_VERSION_NO_V=${DOC_VERSION//v/}
 curl -LO https://github.com/thewtex/ITKDoxygen/releases/download/${DOC_VERSION}/InsightDoxygenDocHtml-${DOC_VERSION_NO_V}.tar.zst
 
+mkdir -p $READTHEDOCS_OUTPUT
 unzstd --long=31 ./InsightDoxygenDocHtml-${DOC_VERSION_NO_V}.tar.zst
-mkdir -p $READTHEDOCS_OUTPUT/html
 tar xf \
   ./InsightDoxygenDocHtml-${DOC_VERSION_NO_V}.tar \
-  --strip-components=1 \
-  -C $READTHEDOCS_OUTPUT/html
+  -C $READTHEDOCS_OUTPUT
 
