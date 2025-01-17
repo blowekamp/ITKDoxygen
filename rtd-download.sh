4dc98ea771545404fcbd290fcc084bb5ae1ed0ce
Matt McCormick <matt@mmmccormick.com>
2025-01-17 07:54:51 -0500

    ENH: Update GitHub Org to InsightSoftwareConsortium
    From development fork.

--- rtd-download.sh
+++ rtd-download.sh
@@ -4,7 +4,7 @@ set -eox
 
 DOC_VERSION=$(if [[ "${READTHEDOCS_GIT_IDENTIFIER}" == *v* ]]; then echo "${READTHEDOCS_GIT_IDENTIFIER}"; else echo "latest"; fi)
 DOC_VERSION_NO_V=${DOC_VERSION//v/}
-curl -LO https://github.com/thewtex/ITKDoxygen/releases/download/${DOC_VERSION}/InsightDoxygenDocHtml-${DOC_VERSION_NO_V}.tar.zst
+curl -LO https://github.com/InsightSoftwareConsortium/ITKDoxygen/releases/download/${DOC_VERSION}/InsightDoxygenDocHtml-${DOC_VERSION_NO_V}.tar.zst
 
 mkdir -p $READTHEDOCS_OUTPUT
 unzstd --long=31 ./InsightDoxygenDocHtml-${DOC_VERSION_NO_V}.tar.zst
